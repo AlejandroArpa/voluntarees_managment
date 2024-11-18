f4e83a3a3a51db0aa9194d2492884d789e5a34f9
Alejandro Ramirez <alejorp195@hotmail-com>
2024-11-17 23:25:41 -0500

    kick off

--- src/java/com/voluntarees/pesistence/database/DAO/UserDAO.java
+++ src/java/com/voluntarees/pesistence/database/DAO/UserDAO.java
@@ -10,12 +10,17 @@ public class UserDAO {
         String sql= "INSERT INTO users (name, email, password, role) VALUES(?, ?, ?, ?);";
         try(
             Connection conn = DatabaseConnection.getConnetction();
-            PreparedStatement stament = conn.prepareStatement(sql)
+            PreparedStatement statement = conn.prepareStatement(sql)
         ){
-            stament.setString(1, objUser.getName());
-            stament.setString(2, objUser.getEmail());
-            stament.setString(3, objUser.getPassword());
-            stament.setString(4, objUser.getRole().toString());
+            statement.setString(1, objUser.getName());
+            statement.setString(2, objUser.getEmail());
+            statement.setString(3, objUser.getPassword());
+            statement.setString(4, objUser.getRole().toString());
+
+            statement.executeUpdate();
+
+            System.out.println("El usuario se agrego correctamente");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
